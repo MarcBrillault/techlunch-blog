a4161be15dbee2a4377c5e220b7dd385afbb8255
Marc Brillault <marc@brillault.fr>
2017-11-24 15:31:55 +0100

    PostsFactory : create img dir if it doesn't exist

--- database/factories/PostsFactory.php
+++ database/factories/PostsFactory.php
@@ -5,12 +5,16 @@ use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+    $imgDir = resource_path('assets/images/TMP');
+    if (!is_dir($imgDir)) {
+        mkdir($imgDir, 0777, true);
+    }
     $timestamp = time() - rand(0, 5000) * 3600 * 24;
     $date      = Carbon::createFromTimestamp($timestamp);
 
     return [
         'title'      => $faker->realText(50),
-        'image'      => $faker->image(resource_path('assets/images/TMP')),
+        'image'      => $faker->image($imgDir),
         'lead'       => $faker->sentence(),
         'text'       => implode(PHP_EOL . PHP_EOL, $faker->sentences(rand(10, 50))),
         'created_at' => $date,
